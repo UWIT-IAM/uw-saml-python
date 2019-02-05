f0f823ceba9aa2a6b35b1d5a5c7024c286e62713
Jeff Franklin <jpf@uw.edu>
2019-02-05 15:29:47 -0800

    Get mock interface working better with Identity.UW.

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -8,13 +8,14 @@ def flick_mock_flag(monkeypatch):
 
 
 def test_login_redirect():
-    expected = ('mock-login?return_to=%2F&force_authn=False'
+    expected = ('/mock-login?return_to=%2F&force_authn=False'
                 '&idp=urn%3Amace%3Aincommon%3Awashington.edu')
     assert uw_saml2.login_redirect() == expected
 
 
 def test_process_response():
-    post = {'idp': 'urn:mace:incommon:washington.edu', 'foo': 'bar'}
+    post = {'idp': 'urn:mace:incommon:washington.edu',
+            'remote_user': 'javerage@washington.edu', 'uwnetid': 'javerage'}
     expected = {'two_factor': False, **post}
     assert uw_saml2.process_response(post) == expected
 
